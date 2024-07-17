0e2999c549d978098650822e516c877d1fed73c6
Abhishek Bhattu <abhishek.bhattu@rotary.org>
2024-07-17 19:36:52 +0000

    terraform and azure variables

---
+++ main.tf
@@ -0,0 +1,53 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.test-name
+  location = var.loc
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = {
+    environment = "sandbox"
+  }
+}
+
+# this is now an "input" value 
+variable "loc" {
+  type    = string
+  default = "westus2"
+}
+
+variable "test-name" {
+  default = "myrgtest03demo"
+  type    = string
+}
